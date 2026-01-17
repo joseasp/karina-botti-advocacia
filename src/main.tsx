714cbdeeb7b5e186fd85c86a7f7bb1ba8767d6de
Jose Polinario da Silva Paula <joseapolinariodasilvapaula123@gmail.com>
2026-01-17 17:34:38 -0300

    Fix SPA routing on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const redirect = new URLSearchParams(window.location.search).get("redirect");
+if (redirect) {
+  window.history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
